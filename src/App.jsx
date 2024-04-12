1acd952cba43fc76008d12a4b4b8934596599707
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-04-13 03:04:54 +0900

    station4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl] = useState(
+    'https://images.dog.ceo/breeds/affenpinscher/n02110627_11759.jpg',
+  )
   return (
     <>
       <header>
         <h1>ODENDAYO!!</h1>
       </header>
       <div>あなたトトロっていうのね！！</div>
-      <img src="https://images.dog.ceo/breeds/affenpinscher/n02110627_11759.jpg" />
+      <img src={dogUrl} />
     </>
   )
 }
