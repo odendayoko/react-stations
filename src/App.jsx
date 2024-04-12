a0a86caf440ab85b801466a8b489701f0499cae6
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-04-13 02:59:30 +0900

    station3

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,13 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  return <header>ODENDAYO!!</header>
+  return (
+    <>
+      <header>
+        <h1>ODENDAYO!!</h1>
+      </header>
+      <div>あなたトトロっていうのね！！</div>
+      <img src="https://images.dog.ceo/breeds/affenpinscher/n02110627_11759.jpg" />
+    </>
+  )
 }
