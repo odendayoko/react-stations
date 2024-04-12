02879910e3c990ce44dc7483e7827c07e07a4d51
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-04-13 03:24:02 +0900

    station6

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const fetchDogUrl = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random')
+      const responseData = await response.json()
+      setDogUrl(responseData.message)
+    } catch (error) {
+      console.log('error', error)
+      throw error
+    }
+  }
   const FIRST_DOG_URL =
     'https://images.dog.ceo/breeds/affenpinscher/n02110627_11759.jpg'
-
-  const SECOND_DOG_URL =
-    'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg'
 
   const [dogUrl, setDogUrl] = useState(FIRST_DOG_URL)
 
@@ -23,13 +30,7 @@ export const App = () => {
       <div>あなたトトロっていうのね！！</div>
       <img src={dogUrl} />
       <div>
-        <button
-          onClick={() => {
-            setDogUrl(SECOND_DOG_URL)
-          }}
-        >
-          更新
-        </button>
+        <button onClick={() => fetchDogUrl()}>更新</button>
       </div>
     </>
   )
